37e084534692f0b8eec89364a818e8359b576471
Your Name <bigtulahi1@gmaiil.com>
2023-11-27 15:10:31 +0000

    loading screen

--- components/providers/liveblocks-provider.tsx
+++ components/providers/liveblocks-provider.tsx
@@ -21,7 +21,21 @@ export function Room({ children }: { children: ReactNode }) {
         message: "",
       }}
     >
-      <ClientSideSuspense fallback={<div>Loading…</div>}>
+      <ClientSideSuspense
+        fallback={
+          <div className="flex flex-col items-center justify-center w-full h-screen gap-2 font-open">
+            <h1 className="font-bold text-center uppercase">Loading</h1>
+            <div className="flex items-center justify-center">
+              <div className="relative block w-10 h-6 loader-dots">
+                <div className="absolute top-0 my-2.5 w-1.5 h-1.5 rounded-full bg-white opacity-75"></div>
+                <div className="absolute top-0 my-2.5 w-1.5 h-1.5 rounded-full bg-white opacity-75"></div>
+                <div className="absolute top-0 my-2.5 w-1.5 h-1.5 rounded-full bg-white opacity-75"></div>
+                <div className="absolute top-0 my-2.5 w-1.5 h-1.5 rounded-full bg-white opacity-75"></div>
+              </div>
+            </div>
+          </div>
+        }
+      >
         {() => children}
       </ClientSideSuspense>
     </RoomProvider>
